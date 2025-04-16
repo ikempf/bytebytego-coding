833b199dd7ed974652ad62b23ceb0a418ea6131f
Ilja <ilja.kempf@pm.me>
2025-04-16 22:16:27 +0200

    Ex 3: Triplet sum

--- src/test/scala/com/ikempf/twopointers/TripletSumTest.scala
+++ src/test/scala/com/ikempf/twopointers/TripletSumTest.scala
@@ -6,4 +6,10 @@ import munit.FunSuite
 class TripletSumTest extends FunSuite:
   test("Triplet sum should work") {
     assertEquals(tripletSum(Vector(0, -1, 2, -3, 1)), Set((-3, 1, 2), (-1, 0, 1)))
+    assertEquals(tripletSum(Vector()), Set())
+    assertEquals(tripletSum(Vector(0)), Set())
+    assertEquals(tripletSum(Vector(1, -1)), Set())
+    assertEquals(tripletSum(Vector(0, 0, 0)), Set((0, 0, 0)))
+    assertEquals(tripletSum(Vector(1, 0, 1)), Set())
+    assertEquals(tripletSum(Vector(0,0,1,-1, 1, -1)), Set((-1, 0, 1)))
   }
